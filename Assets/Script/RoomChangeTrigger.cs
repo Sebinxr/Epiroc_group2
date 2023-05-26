8810d5d136ba65fdfcbed47874453760615023d0
Zuhaib001 <zuhaib.paracha@hyperisland.se>
2023-05-26 10:54:19 +0200

    Teleportation with Box collider added

---
+++ Assets/Script/RoomChangeTrigger.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class RoomChangeTrigger : MonoBehaviour
+{
+    public Transform room1;
+    public Transform room2;
+
+    private bool isInRoom1 = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("RoomTrigger"))
+        {
+            if (isInRoom1)
+            {
+                TeleportToRoom2();
+            }
+            else
+            {
+                TeleportToRoom1();
+            }
+        }
+    }
+
+    private void TeleportToRoom2()
+    {
+        transform.position = room2.position;
+        transform.rotation = room2.rotation;
+        isInRoom1 = false;
+    }
+
+    private void TeleportToRoom1()
+    {
+        transform.position = room1.position;
+        transform.rotation = room1.rotation;
+        isInRoom1 = true;
+    }
+}
