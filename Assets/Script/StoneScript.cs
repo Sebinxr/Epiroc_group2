c022d9063033db16b91a572860119f789a6653ac
Zuhaib001 <zuhaib.paracha@hyperisland.se>
2023-06-05 14:45:38 +0200

    OnTrigger Stone and Added Score text with it

--- Assets/Script/StoneScript.cs
+++ Assets/Script/StoneScript.cs
@@ -4,38 +4,15 @@ using UnityEngine;
 
 public class StoneScript : MonoBehaviour
 {
-    public int points = 10;
-    public AudioClip collectSound; // The audio clip for the collect sound effect
-
-    private AudioSource audioSource; // Reference to the AudioSource component
-
-    private void Start()
-    {
-        // Get the AudioSource component attached to the same game object
-        audioSource = GetComponent<AudioSource>();
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
+
+        if (playerInventory != null)
         {
-            // Access the player's score script or manager and update the score
-            ScoreManager.Instance.AddPoints(points);
-
-            // Play the collect sound effect
-            PlayCollectSound();
-
-            // Destroy the stone object
             Destroy(gameObject);
-        }
-    }
+            playerInventory.CollectKey();
 
-    private void PlayCollectSound()
-    {
-        // Check if an audio clip is assigned and there is an AudioSource component
-        if (collectSound != null && audioSource != null)
-        {
-            audioSource.PlayOneShot(collectSound);
         }
     }
 }
