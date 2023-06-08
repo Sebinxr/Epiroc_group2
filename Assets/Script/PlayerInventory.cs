f3bfdf6d6db9e2687f4bff3549079c051840bef0
Zuhaib001 <zuhaib.paracha@hyperisland.se>
2023-06-08 02:30:26 +0200

    Teleportation lock is working

--- Assets/Script/PlayerInventory.cs
+++ Assets/Script/PlayerInventory.cs
@@ -7,6 +7,9 @@ public class PlayerInventory : MonoBehaviour
 {
     private int keyCount;
     public TextMeshProUGUI keyCountText;
+    public GameObject teleportationObject; // Reference to the teleportation script or object
+
+    private bool isTeleportationUnlocked = false; // Variable to track the teleportation unlock status
 
     private void Start()
     {
@@ -18,6 +21,19 @@ public class PlayerInventory : MonoBehaviour
     {
         keyCount++;
         UpdateKeyCountText();
+
+        // Check if the player has collected two stones
+        if (keyCount >= 10)
+        {
+            UnlockTeleportation();
+        }
+    }
+
+    private void UnlockTeleportation()
+    {
+        isTeleportationUnlocked = true;
+        // Enable the teleportation functionality or activate the teleportation script
+        teleportationObject.SetActive(true);
     }
 
     private void UpdateKeyCountText()
